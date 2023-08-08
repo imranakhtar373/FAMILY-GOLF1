a3654c115299c0f60fb562c17082ac13ebba0dfd
imranakhtar373 <imranakhtar3738@gmail.com>
2023-08-08 13:58:32 +0000

    Added some js for cursor pointer and cursor blur background

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-var crsr = document.querySelector("cursor");
-document.addEventListener("mousemove", function (dets) {
-  crsr.style.left = dets.x + "px";
-  crsr.style.top = dets.y + "px";
-  
-})
+var cursor = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove",function(dets){
+  cursor.style.left = dets.x+"px",
+  cursor.style.top = dets.y+"px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
 
 
 
